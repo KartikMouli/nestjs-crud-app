832c08bea31fd417128bf0301c8257f80dc0d267
KartikMouli <kartikmouli0@gmail.com>
2025-04-01 13:52:45 +0530

    add user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './users.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) { }
+
+    @Get()
+    getAllUsers() {
+        return this.userService.findAll()
+    }
+
+
+    @Get(':id')
+    getUser(@Param('id') id: string) {
+        return this.userService.findOne(Number(id))
+    }
+
+    @Post()
+    createUser(@Body() user: User) {
+        return this.userService.create(user)
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id') id: string) {
+        return this.userService.delete(Number(id));
+    }
+}
