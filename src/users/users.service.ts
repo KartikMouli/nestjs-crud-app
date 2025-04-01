7a0290a4c7c0d999a318ec28fb8733632650da68
KartikMouli <kartikmouli0@gmail.com>
2025-04-01 13:46:35 +0530

    add user service (provider)

---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+
+@Injectable()
+export class UsersService {
+    private users: User[] = [];
+
+
+    findAll(): User[] {
+        this.users;
+    }
+
+    findOne(id: number): User | undefined {
+        return this.users.find(user => user.id === id)
+    }
+
+    create(user: User) {
+        user.id = this.users.length + 1;
+        this.users.push(user)
+        return user
+    }
+
+    delete(id: number): boolean {
+        const idx = this.users.findIndex(user => user.id === id);
+        if (idx === -1) return false;
+
+        this.users.splice(idx, 1);
+        return true;
+    }
+
+    
+
+}
